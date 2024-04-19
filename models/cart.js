593482465a506b4b3e5bd89cb2bcabe79bdad4e1
Dziekhey <dakeysandra@gmail.com>
2024-04-19 15:28:48 +0000

    remodelling done

--- models/cart.js
+++ models/cart.js
@@ -1,15 +1,9 @@
 import { Schema, model } from "mongoose";
+import mongoose from "mongoose";
 
 const cartSchema = new Schema({
-    name:{type: String, required: true},
-    image: {type: String, required: true},
-    price: {type: Number, required: true},
-    category: {type: Number, required: true},
-    discount: {type: Number},
-    color: {String},
-    size: {String},
-    total_item: {type: Number, required: true},
-    total_price: {type: Number, required: true},
+    productId: { type: mongoose.Schema.Types.ObjectId, required: true},
+    user_name: { type: String, required: true }
 });
 
 export const CartModel = model('Cart', cartSchema, 'carts');
--- models/orders.js
+++ models/orders.js
@@ -1,18 +1,13 @@
 import { Schema, model } from "mongoose";
+import mongoose from "mongoose";
 
 const orderSchema = new Schema({
-    name:{type: String, required: true},
-    image: {type: String, required: true},
-    price: {type: Number, required: true},
-    category: {type: String, required: true},
-    discount: {type: Number},
-    color: {String},
-    size: {String},
-    total_item: {type: Number, required: true},
-    total_price: {type: Number, required: true},
-    orderStatus: {type: String},
-    paymentMethod: {type: String, required: true},
-    seller: {type: String, required: true},
+  productId: { type: mongoose.Schema.Types.ObjectId, required: true},
+  total_item: { type: Number, required: true },
+  total_price: { type: Number, required: true },
+  orderStatus: { type: String },
+  paymentMethod: { type: String, required: true },
+  user_name: { type: String, required: true },
 });
 
-export const OrderModel = model('Order', orderSchema, 'orders');
+export const OrderModel = model("Order", orderSchema, "orders");
